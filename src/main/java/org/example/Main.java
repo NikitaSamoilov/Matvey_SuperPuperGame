76efa8ac502283358f14a0719faf2e8adbbe1c54
matvey <m.chibisoff.work@yandex.ru>
2025-03-12 21:12:44 +0300

    Создание цикла игры

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // запросить имя пользователя
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String userName = sc.nextLine();
+        System.out.println(userName);
 
+        boolean isSuccess = true;
+        while (isSuccess) {
+            System.out.println("Угадай число от 1 до 10");
+            int successNumb = (int)Math.random()*10;
+            int numb = sc.nextInt();
+            System.out.println("Результат: " + successNumb + " " + numb);
+        }
         //начать бесконечный цикл проверок угадает или нет, если угадал завершить игру и похвалить
         //при неугадовании сказать в меньшую или большую сторону не угадал
     }
