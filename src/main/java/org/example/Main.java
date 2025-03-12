b752cf3e05f9bd6edfff77d65ab1e4a066030336
Никита Самойлов <nswork87@yandex.ru>
2025-03-12 20:41:29 +0400

    Test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        // запросить имя пользователя
+
+        //начать бесконечный цикл проверок угадает или нет, если угадал завершить игру и похвалить
+        //при неугадовании сказать в меньшую или большую сторону не угадал
     }
 }
